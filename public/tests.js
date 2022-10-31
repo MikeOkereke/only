48a0eb5b49ec480f798510c91b0f9fc5edc2bc13
MikeOkereke <michaelokereke4u@gmail.com>
2022-10-31 20:05:26 +0100

    Video-04-Create Git Repository

---
+++ public/tests.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import moduleName from 'module';
+
+function App() {
+  return <div className="test">Hello world</div>;
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ONLY</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
